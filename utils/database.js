78293b630e82c6dcf44ec869a404a56f6825ec7d
Adam Purcell <apurcell88@gmail.com>
2024-01-16 15:06:49 -0600

    Trying to add vercel/mongo compatability

--- utils/database.js
+++ utils/database.js
@@ -1,29 +1,28 @@
 import mongoose from 'mongoose';
-// import Game from '@/models/game'
 
 let isConnected = false // track the connection status
 
-// export const connectToDB = async () => {
-//   // sets mongoose options
-//   mongoose.set('strictQuery', true);
+export const connectToDB = async () => {
+  // sets mongoose options
+  mongoose.set('strictQuery', true);
 
-//   if (isConnected) {
-//     // console.log('MongoDB is already connected');
-//     return;
-//   }
+  if (isConnected) {
+    // console.log('MongoDB is already connected');
+    return;
+  }
 
-//   try {
-//     await mongoose.connect(process.env.MONGODB_URI, {
-//       dbName: 'game_store',
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: 'game_store',
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-//     isConnected = true;
+    isConnected = true;
 
-//     console.log('MongoDB connected');
+    console.log('MongoDB connected');
     
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
+  } catch (err) {
+    console.error(err);
+  }
+}
